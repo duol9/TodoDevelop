d3de4c31bcae095c72b89e9f13c744436e02afd9
duol9 <lhyy0195@gmail.com>
2024-12-17 19:50:20 +0900

    feat: TodoEntity 작성자명 제외
    유저 Entity와 연관관계가 형성돼 작성자명 대신 유저 고유 식별자 사용에 따라 삭제

--- src/main/java/com/example/tododevelop/dto/TodoResponseDto.java
+++ src/main/java/com/example/tododevelop/dto/TodoResponseDto.java
@@ -7,14 +7,12 @@ import java.time.LocalDate;
 
 @Getter
 public class TodoResponseDto {
-    private String userName;
     private String title;
     private String contents;
     private LocalDate createdAt;
     private LocalDate modifiedAt;
 
     public TodoResponseDto(TodoEntity todoEntity){
-        this.userName = todoEntity.getUserName();
         this.title = todoEntity.getTitle();
         this.contents = todoEntity.getContents();
         this.createdAt = todoEntity.getCreatAt();
