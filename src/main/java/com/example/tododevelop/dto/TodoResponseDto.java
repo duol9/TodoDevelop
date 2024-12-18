4391d89e4c94eab4087d6ee099366dbeb087af04
duol9 <lhyy0195@gmail.com>
2024-12-18 19:29:34 +0900

    feat: 할일 API 응답 시 작성자명 확인 가능

--- src/main/java/com/example/tododevelop/dto/TodoResponseDto.java
+++ src/main/java/com/example/tododevelop/dto/TodoResponseDto.java
@@ -7,12 +7,14 @@ import java.time.LocalDate;
 
 @Getter
 public class TodoResponseDto {
+    private String name;
     private String title;
     private String contents;
     private LocalDate createdAt;
     private LocalDate modifiedAt;
 
     public TodoResponseDto(TodoEntity todoEntity){
+        this.name = todoEntity.getUserEntity().getUserName();
         this.title = todoEntity.getTitle();
         this.contents = todoEntity.getContents();
         this.createdAt = todoEntity.getCreatAt();
